f7fa73de4b08da9db1b3fd3b5e6c4fa3f887a440
RAMESH DUMALA <rameshtelecomengineer@gmail.com>
2017-02-27 18:08:16 +0530

    Add files via upload

---
+++ TCPCLIENTIP.ino
@@ -0,0 +1,122 @@
+
+
+#include <ESP8266WiFi.h>
+
+char ssid[] = "RES_Research"; //  your network SSID (name)
+char pass[] = "aruba567";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+void printWiFiStatus();
+// use the numeric IP instead of the name for the server:
+IPAddress server(172, 16, 16, 150); // numeric IP for Google (no DNS)
+
+char mes2[200];
+char key2[100];
+char key[] = "kgdjgdlGAJJ";
+int j = 0, k = 0, len1 = 0,n=0;
+char mes1[200];
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+
+
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(ssid, pass);
+
+  // wait 10 seconds for connection:
+  delay(10000);
+
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+     client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+     client.println("Connection: close");
+    client.println();
+  }
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+    mes1[n]=c;
+    n++;
+  }
+  n=0;
+  // if the server's disconnected, stop the client:
+ 
+  if (!client.connected()) {
+     decodeMsg();
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+void decodeMsg()
+{
+  k=0;j=0;
+  len1 = strlen(key);
+    Serial.println(mes1);
+  for (int i = 0; i <= 300; i++)
+  {
+    if (i % 2 == 0)
+    {
+      mes2[j] = mes1[i];
+      j++;
+    }
+
+    else
+    {
+      key2[k] = mes1[i];
+      k++;
+    }
+
+  }
+  Serial.println("after decoded");
+  Serial.println("mes2");
+  Serial.println(mes2);
+  Serial.println("key2");
+  Serial.println(key2);
+  if (!strncmp(key2, key, len1))
+  {
+    Serial.println("in if condtion");
+  }
+
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
---
+++ TCPSERVERIP.ino
@@ -0,0 +1,152 @@
+#include <ESP8266WiFi.h>
+
+
+char ssid[] = "RES_Research";      // your network SSID (name)
+char pass[] = "aruba567";          // your network password
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+
+IPAddress ip(172, 16, 16, 150);
+IPAddress gateway(172, 16, 16, 16);
+IPAddress subnet(255, 255, 0, 0);
+
+
+WiFiServer server(80);
+void printWiFiStatus() ;
+
+char key[50] = "kgdjgdlGAJJ";
+char mes[100];
+int j = 0, k = 0, l = 0, i = 0;
+char mes1[200];
+char ch;
+int len1, len2;
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+  WiFi.config(ip, gateway, subnet);
+  WiFi.begin(ssid, pass);
+
+  server.begin();
+  printWiFiStatus();
+}
+
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.print("<br>");
+          msgSend();
+          client.print(mes1);
+          client.println("<br />");
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void msgSend()
+{
+  Serial.println("please enter the data");
+  memset(mes1, 0, 200);
+  memset(mes, 0, 100);
+  ch = '0';
+  i = 0, l = 0, j = 0, k = 0;
+  while (ch != '\n')
+  {
+    if (Serial.available() > 0) {
+
+      ch = Serial.read(); // read the incoming byte:
+      mes[l] = ch;
+      l++;
+    }
+
+  }
+  len1 = strlen(mes);
+  len2 = strlen(key);
+  len1 = 2 * len1 - 4;
+  Serial.println(mes);
+  while (1)
+  {
+
+    if (i % 2 == 0)
+    {
+      mes1[i] = mes[j];
+      j++;
+      if (i == len1)
+        break;
+    }
+
+    else
+    {
+      if (i == len1)
+        break;
+      else if (k == len2)
+      {
+        k = 0;
+      }
+      mes1[i] = key[k];
+      k++;
+
+    }
+    i++;
+
+  }
+
+}
